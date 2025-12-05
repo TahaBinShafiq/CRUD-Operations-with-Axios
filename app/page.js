188eca159395e305a4c18ba29dfdd02114adceb6
Taha Bin Shafiq <muhammadtahaaa7@gmail.com>
2025-12-05 19:58:37 +0500

    add tenstack Querry

--- app/page.js
+++ app/page.js
@@ -1,8 +1,19 @@
+'use client'
+
 import Image from "next/image";
 import Page from "./dashboard/page";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 
 export default function Home() {
+  const queryClient = new QueryClient();
+
   return (
-   <Page/>
+    <QueryClientProvider client={queryClient}>
+      <Page />
+    </QueryClientProvider>
   );
 }
--- app/pages-partials/dashoboard/index.js
+++ app/pages-partials/dashoboard/index.js
@@ -1,183 +1,217 @@
 "use client";
-import { addPost, deleteProductData, getproductData, updatePost } from "@/app/services"
+import {
+  addPost,
+  deleteProductData,
+  getproductData,
+  updatePost,
+} from "@/app/services";
 import { RippleButton } from "@/components/ui/ripple-button";
-import { useEffect, useState } from "react"
-
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 
 function DashboardPartial() {
-    const [productData, setProductData] = useState([])
-    const [emptyInput, setEmptyInput] = useState(false)
+  const [emptyInput, setEmptyInput] = useState(false);
+  const [deletingId, setDeletingId] = useState(null);
+  const [addUserData, setAddUserData] = useState({
+    name: "",
+    description: "",
+  });
+  const [editId, setEditId] = useState(null);
+  const [editUserData, setEditUserData] = useState({
+    name: "",
+    description: "",
+  });
 
-    const [addUserData, setAddUserData] = useState({
-        name: '',
-        description: ''
-    })
+  const queryClient = useQueryClient();
 
-    const [editId, setEditId] = useState(null)
-    const [editUserData, setEditUserData] = useState({
-        name: '',
-        description: ''
-    })
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["fetchData"],
+    queryFn: () => getproductData(),
+  });
 
+  console.log(addUserData);
 
+  const mutationAdd = useMutation({
+    mutationFn: (newPost) => addPost(newPost),
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["fetchData"] });
+    },
+  });
 
-    const editData = ({ id: editId, name: editName, description: editDis }) => {
-        setEditId(editId)
-        setEditUserData({
-            name: editName,
-            description: editDis
-        })
-        console.log(editId)
-        console.log(editName)
-        console.log(editDis)
-    }
-    console.log(editId)
+  const mutationDelete = useMutation({
+    mutationFn: (id) => deleteProductData(id),
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["fetchData"] });
+    },
+  });
 
-    const submitEditData = async (editId) => {
-        if (!editUserData.name || !editUserData) {
-            alert("Enter Data")
-            return
-        }
-        console.log(editId)
-        const update = await updatePost(editId, editUserData)
-        setEditUserData({ name: "", description: "" })
-        setEditId(null)
-        await getData()
-    }
+  const mutationEdit = useMutation({
+      mutationFn: (data) => updatePost(editId, data),
+      onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["fetchData"] });
+    },
+  });
 
+  console.log(data);
 
-    const addData = async () => {
-        if (!addUserData.name || !addUserData.description) {
-            setEmptyInput(true);
-            return;
-        }
+  return (
+    <section>
+      <div>
+        <h1 className="text-[30px] font-bold text-center">
+          CRUD Operations Using Axios
+        </h1>
+      </div>
 
-        setEmptyInput(false)
+      <div className="lg:w-[800px] mx-auto mt-[20px] p-4 sm:p-6 md:p-5 bg-white rounded-2xl shadow-lg space-y-4 sm: w-[90%]">
+        <h2 className="text-xl font-semibold text-center">
+          {editId === null ? "Add Details" : "Edit Details"}
+        </h2>
 
-        const newPost = await addPost(addUserData)
-        const { data: newData } = newPost
-        const { data } = newData
-        console.log(data.id)
-        setProductData([...productData, data])
+        <div className="lg:flex flex-row w-full justify-start gap-[15px] sm:flex flex-col gap[40px]">
+          <div className="flex flex-col w-full space-y-1">
+            <label className="text-sm font-medium">Name</label>
+            <input
+              type="text"
+              placeholder="Enter name"
+              value={editId === null ? addUserData.name : editUserData.name}
+              onChange={(e) => {
+                if (editId === null) {
+                  setAddUserData({ ...addUserData, name: e.target.value });
+                } else {
+                  setEditUserData({ ...editUserData, name: e.target.value });
+                }
+              }}
+              className={
+                emptyInput && !addUserData.name
+                  ? "border border-red-500 rounded-[5px] p-2 focus:outline-none focus:ring-2 focus:ring-red-500"
+                  : "border rounded-[5px] p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              }
+            />
+          </div>
 
-        setAddUserData({
-            name: '',
-            description: ''
-        })
-        console.log("add")
-        console.log(productData)
-    }
+          {/* Description Input */}
+          <div className="flex flex-col space-y-1 w-full">
+            <label className="text-sm font-medium">Description</label>
+            <input
+              type="text"
+              placeholder="Enter description"
+              value={
+                editId === null
+                  ? addUserData.description
+                  : editUserData.description
+              }
+              onChange={
+                editId === null
+                  ? (event) =>
+                      setAddUserData({
+                        ...addUserData,
+                        description: event.target.value,
+                      })
+                  : (event) => {
+                      setEditUserData({
+                        ...editUserData,
+                        description: event.target.value,
+                      });
+                    }
+              }
+              className={
+                emptyInput && !addUserData.name
+                  ? "border border-red-500 rounded-[5px] p-2 focus:outline-none focus:ring-2 focus:ring-red-500"
+                  : "border rounded-[5px] p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              }
+            />
+          </div>
+          <button
+            onClick={() => {
+              if (
+                editId === null &&
+                (!addUserData.name || !addUserData.description)
+              ) {
+                return setEmptyInput(true);
+              }
 
-
-    const getData = async () => {
-        const data = await getproductData()
-        setProductData(data)
-
-    }
-
-
-    useEffect(() => {
-        getData()
-    }, [])
-    console.log(addUserData)
-
-
-    const deleteProduct = async (id) => {
-        console.log(id)
-        const deleteData = await deleteProductData(id)
-        setProductData(deleteData)
-        await getData()
-    }
-
-
-
-
-    return <section>
-        <div>
-            <h1 className="text-[30px] font-bold text-center">CRUD Operations Using Axios</h1>
+              if (
+                editId !== null &&
+                (!editUserData.name || !editUserData.description)
+              ) {
+                return setEmptyInput(true);
+              }
+              setEmptyInput(false);
+              {
+                editId
+                  ? mutationEdit.mutate(editUserData, {
+                      onSuccess: () => {
+                        setEditId(null);
+                        setEditUserData({ name: "", description: "" });
+                      },
+                    })
+                  : mutationAdd.mutate(addUserData, {
+                      onSuccess: () =>
+                        setAddUserData({ name: "", description: "" }),
+                    });
+              }
+              setAddUserData({ name: "", description: "" });
+            }}
+            className="lg:w-[150px] h-[47px] mt-[20px] cursor-pointer bg-blue-600 text-white py-2 rounded-xl hover:bg-blue-700 transition-all duration-200 md:w-[150px] sm: w-full"
+          >
+            {editId === null ? "Add" : mutationAdd.isPending ? "Adding..." : mutationEdit.isPending ? "updating..." : "Update"}
+          </button>
         </div>
+      </div>
 
-        <div className="lg:w-[800px] mx-auto mt-[20px] p-4 sm:p-6 md:p-5 bg-white rounded-2xl shadow-lg space-y-4 sm: w-[90%]">
-            <h2 className="text-xl font-semibold text-center">{editId === null ? "Add Details" : "Edit Details"}</h2>
-
-
-            {/* Name Input */}
-            <div className="lg:flex flex-row w-full justify-start gap-[15px] sm:flex flex-col gap[40px]">
-
-                <div className="flex flex-col w-full space-y-1">
-                    <label className="text-sm font-medium">Name</label>
-                    <input
-                        type="text"
-                        placeholder="Enter name"
-                        value={editId === null ? addUserData.name : editUserData.name}
-                        onChange={editId === null ? (event) => setAddUserData({ ...addUserData, name: event.target.value }) :
-                            (event) => setEditUserData({ ...editUserData, name: event.target.value })
-                        }
-                        className={emptyInput && !addUserData.name
-                            ? "border border-red-500 rounded-[5px] p-2 focus:outline-none focus:ring-2 focus:ring-red-500"
-                            : "border rounded-[5px] p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        }
-                    />
-                </div>
-
-
-                {/* Description Input */}
-                <div className="flex flex-col space-y-1 w-full" >
-                    <label className="text-sm font-medium">Description</label>
-                    <input
-                        type="text"
-                        placeholder="Enter description"
-                        value={editId === null ? addUserData.description : editUserData.description}
-                        onChange={editId === null ? (event) => setAddUserData({ ...addUserData, description: event.target.value }) :
-                            (event) => { setEditUserData({ ...editUserData, description: event.target.value }) }
-                        }
-                        className={emptyInput && !addUserData.name
-                            ? "border border-red-500 rounded-[5px] p-2 focus:outline-none focus:ring-2 focus:ring-red-500"
-                            : "border rounded-[5px] p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        }
-                    />
-                </div>
-                <button
-                    onClick={editId === null ? addData : () => { submitEditData(editId) }}
-                    className="lg:w-[150px] h-[47px] mt-[20px] cursor-pointer bg-blue-600 text-white py-2 rounded-xl hover:bg-blue-700 transition-all duration-200 md:w-[150px] sm: w-full">
-                    {editId === null ? "Add" : "Update"}
-                </button>
-
-            </div>
-
-        </div>
-        
+      {isLoading ? (
+        <>
+          <div className="text-center pt-[40px]">Loading...</div>
+        </>
+      ) : (
         <div className="mx-w-[1250px] mx-auto p-4">
-        <div className="flex flex-wrap justify-start gap-4" >
-            {productData.length > 0 && productData.map((product) => {
-                const { name, description, id } = product
+          <div className="flex flex-wrap justify-start gap-4">
+            {data?.length > 0 &&
+              data?.map((product) => {
+                const { name, description, id } = product;
                 return (
-                    <div key={id} className="w-full bg-white shadow-lg rounded-2xl p-4 border border-gray-200 hover:shadow-xl transition-shadow duration-300 sm:max-w-[300px] lg:min-w-[270px] lg:max-w-[300px] ">
-                        <h2 className="text-2xl font-semibold text-black mb-2">
-                            {name}
-                        </h2>
-                        <p className="text-gray mb-4">
-                            {description}
-                        </p>
+                  <div
+                    key={id}
+                    className="w-full bg-white shadow-lg rounded-2xl p-4 border border-gray-200 hover:shadow-xl transition-shadow duration-300 sm:max-w-[300px] lg:min-w-[270px] lg:max-w-[300px] "
+                  >
+                    <h2 className="text-2xl font-semibold text-black mb-2">
+                      {name}
+                    </h2>
+                    <p className="text-gray mb-4">{description}</p>
 
-                        <div className="flex justify-end gap-3">
-                            <RippleButton rippleColor="#ADD8E6" onClick={() => {
-                                editData(product)
-                            }}>Edit</RippleButton>
-                            <button onClick={() => {
-                                deleteProduct(id)
-                            }} className="px-4 py-2 cursor-pointer bg-red-500 text-white rounded-lg hover:bg-red-600 transition-colors">
-                                Delete
-                            </button>
-                        </div>
+                    <div className="flex justify-end gap-3">
+                      <RippleButton
+                        rippleColor="#ADD8E6"
+                        onClick={() => {
+                          setEditId(id);
+                          setEditUserData(product);
+                        }}
+                      >
+                        Edit
+                      </RippleButton>
+                      <button
+                        onClick={() => {
+                          setDeletingId(id);
+                          mutationDelete.mutate(id, {
+                            onSettled: () => setDeletingId(null),
+                          });
+                        }}
+                        className="px-4 py-2 cursor-pointer bg-red-500 text-white rounded-lg hover:bg-red-600 transition-colors"
+                      >
+                        {deletingId === id ? "Deleting..." : "Delete"}
+                      </button>
                     </div>
-                )
-            })}
+                  </div>
+                );
+              })}
+          </div>
         </div>
-        </div>
-
+      )}
     </section>
+  );
 }
 
-
-export default DashboardPartial
+export default DashboardPartial;
