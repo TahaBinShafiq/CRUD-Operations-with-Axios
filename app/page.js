fdb90a46777e8e274b3095a10ce95739ac481f83
Taha Bin Shafiq <muhammadtahaaa7@gmail.com>
2025-12-05 20:26:00 +0500

    correct error

--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
-import Providers from "@/provider";
 import Page from "./dashboard/page";
 
 export default function Home() {
   return (
-    <Providers>
       <Page />
-    </Providers>
   );
 }
